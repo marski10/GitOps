40461fee3b26cacaafa39fff0415fc0a02526d90
marski10 <marquinhus.bill@hotmail.com>
2024-08-23 04:17:25 +0000

    ajustado nome do argo

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<h1> Hello World </h1>")
+		fmt.Fprintf(w, "<h1> Hello Argo </h1>")
 	})
 
 	http.ListenAndServe(":8080", nil)
